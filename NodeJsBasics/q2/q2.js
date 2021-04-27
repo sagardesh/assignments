99fa19bc21236587fa73244df80cc07b0238b940
Sagar Deshpande <sagpandu@gmail.com>
2021-04-27 19:13:51 +0530

    renamed files

--- NodeJsBasics/q2/q2.js
+++ NodeJsBasics/q2/q2.js
@@ -6,7 +6,7 @@ var template = fs.readFileSync('template.js','utf8');
 var compliedTemplate = handlebars.compile(template);
 
 //get json data
-const data = JSON.parse(fs.readFileSync('data.json','utf8'));
+const data = JSON.parse(fs.readFileSync('input.json','utf8'));
 
 //template against data
 var result = compliedTemplate(data)
